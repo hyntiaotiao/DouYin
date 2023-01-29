cf4491c1993575be46792c3fd77d04d8cf31b079
daorongxing <12044046+yanqingzheng@user.noreply.gitee.com>
2023-01-29 09:35:35 +0800

    增加最大视频上传容量

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func initRouter(r *gin.Engine) {
 	//apiRouter.GET("/user/", controller.UserInfo)
 	apiRouter.POST("/user/register/", controller.Register)
 	apiRouter.POST("/user/login/", controller.Login)
+	r.MaxMultipartMemory = 128 << 20 //设置视频最大上传容量
 	apiRouter.POST("/publish/action/", controller.Publish)
 	//apiRouter.GET("/publish/list/", controller.PublishList)
 	//
